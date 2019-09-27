6f3d07bdd7b500d7b1041466b40c863108c23180
Jack Lau <jackpocket@gmail.com>
2019-09-27 13:11:49 +0800

    Config Ruby env

--- ruby/install.sh
+++ ruby/install.sh
@@ -9,6 +9,14 @@ fi
 # Install ruby binaries
 brew bundle --file=$DOTFILES/ruby/Brewfile
 
+rbenv install 2.5.1
+rbenv --global 2.5.1
+
+source $DOTFILES/config/ruby/.rubyrc
+cd $DOTFILES/config; stow ruby -t ~/; cd $DOTFILES
+
 # Install Bundler
 gem install bundler
 gem update --system
+
+rbenv rehash
